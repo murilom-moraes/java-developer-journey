d2fa464b165592125d327fd79f33eae014eb632c
Murilo M. Moraes <murilom.moraes@pm.me>
2026-06-27 23:10:52 -0300

    docs: add exercise description to circle area calculator

--- src/exercises/sequential/CircleAreaCalculator.java
+++ src/exercises/sequential/CircleAreaCalculator.java
@@ -3,6 +3,15 @@ package exercises.sequential;
 import java.util.Locale;
 import java.util.Scanner;
 
+/*
+ * Calculadora de Área de Círculo
+ * 
+ * Descrição:
+ * Programa que lê o valor do raio de um círculo e calcula 
+ * sua área utilizando pi = 3.14159, com saída formatada 
+ * para quatro casas decimais.
+ */
+
 public class CircleAreaCalculator {
 
 	public static void main(String[] args) {
