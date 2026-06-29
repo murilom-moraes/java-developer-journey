f9500a3014d49093ef9aa91a4daa760285420554
Murilo M. Moraes <murilom.moraes@pm.me>
2026-06-28 21:00:34 -0300

    feat: implement even or odd checker exercise

---
+++ src/exercises/conditional/EvenOrOddChecker.java
@@ -0,0 +1,33 @@
+package exercises.conditional;
+
+import java.util.Scanner;
+
+/*
+ * Verificador de Par ou Ímpar
+ * 
+ * Descrição:
+ * Fazer um programa para ler um número inteiro e dizer 
+ * se este número é par ou ímpar.
+ */
+
+public class EvenOrOddChecker {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int numero;
+		
+		numero = sc.nextInt();
+		
+		if(numero % 2 == 0) {
+			System.out.println("PAR");
+		} else {
+			System.out.println("IMPAR");
+		}
+
+		sc.close();
+		
+	}
+
+}
