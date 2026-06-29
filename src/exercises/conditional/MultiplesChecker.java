09d4b62431d0b8fd7051f72dd8af99618ed6df41
Murilo M. Moraes <murilom.moraes@pm.me>
2026-06-28 21:00:48 -0300

    feat: implement multiples checker exercise

---
+++ src/exercises/conditional/MultiplesChecker.java
@@ -0,0 +1,36 @@
+package exercises.conditional;
+
+import java.util.Scanner;
+
+/*
+ * Verificador de Números Múltiplos
+ * 
+ * Descrição:
+ * Leia 2 valores inteiros (A e B). Após, o programa deve mostrar uma 
+ * mensagem "Sao Multiplos" ou "Nao sao Multiplos", indicando se os 
+ * valores lidos são múltiplos entre si. Os números devem poder ser 
+ * digitados em ordem crescente ou decrescente.
+ */
+
+public class MultiplesChecker {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int a, b;
+		
+		a = sc.nextInt();
+		b = sc.nextInt();
+		
+		if((a % b == 0) || (b % a == 0)) {
+			System.out.println("Sao Multiplos");
+		} else {
+			System.out.println("Nao sao Multiplos");
+		}
+
+		sc.close();
+		
+	}
+
+}
