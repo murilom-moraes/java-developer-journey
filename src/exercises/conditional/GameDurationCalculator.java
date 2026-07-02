080790b427466acd5aa02e84fd964a6c93df5bff
Murilo M. Moraes <murilom.moraes@pm.me>
2026-07-02 19:21:38 -0300

    feat: implement game duration calculator exercise

---
+++ src/exercises/conditional/GameDurationCalculator.java
@@ -0,0 +1,28 @@
+package exercises.conditional;
+
+import java.util.Scanner;
+
+public class GameDurationCalculator {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int horaInicial = sc.nextInt();
+		int horaFinal = sc.nextInt();
+		
+		int duracao;
+		
+		if (horaInicial < horaFinal) {
+			duracao = horaFinal - horaInicial;
+		} else {
+			duracao = (24 - horaInicial) + horaFinal;
+		}
+		
+		System.out.println("O JOGO DUROU " + duracao + " HORAS(S)");
+		
+		sc.close();
+
+	}
+
+}
