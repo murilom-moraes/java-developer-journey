420e1f7e933e9d9e8a3db0729def932b423bb650
Murilo M. Moraes <murilom.moraes@pm.me>
2026-07-03 00:15:42 -0300

    docs: add exercise description to QuadrantChecker

--- src/exercises/conditional/QuadrantChecker.java
+++ src/exercises/conditional/QuadrantChecker.java
@@ -3,6 +3,15 @@ package exercises.conditional;
 import java.util.Locale;
 import java.util.Scanner;
 
+/*
+ * Verificador de Quadrante
+ * 
+ * Descrição:
+ * Leia 2 valores com uma casa decimal (x e y), que devem representar as coordenadas
+ * de um ponto em um plano. A seguir, determine qual o quadrante ao qual pertence o
+ * ponto, ou se está sobre um dos eixos cartesianos ou na origem (x = y = 0).
+ */
+
 public class QuadrantChecker {
     public static void main(String[] args) {
         
