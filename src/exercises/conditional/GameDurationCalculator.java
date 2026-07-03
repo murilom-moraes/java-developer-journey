a1f251d7631704ba870a6097e47e4c5c5bc36f95
Murilo M. Moraes <murilom.moraes@pm.me>
2026-07-03 00:13:11 -0300

    docs: add exercise description to GameDurationCalculator

--- src/exercises/conditional/GameDurationCalculator.java
+++ src/exercises/conditional/GameDurationCalculator.java
@@ -2,6 +2,16 @@ package exercises.conditional;
 
 import java.util.Scanner;
 
+/*
+ * Calculadora de Duração de Jogo
+ * 
+ * Descrição:
+ * Leia a hora inicial e a hora final de um jogo. A seguir calcule 
+ * a duração do jogo, sabendo que o mesmo pode começar em um dia 
+ * e terminar em outro, tendo uma duração mínima de 1 hora e 
+ * máxima de 24 horas.
+ */	
+
 public class GameDurationCalculator {
 
 	public static void main(String[] args) {
